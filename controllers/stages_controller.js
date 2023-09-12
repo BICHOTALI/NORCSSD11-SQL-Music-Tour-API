56a3806b4307ae2b7069ad0066ccd99b9b85566a
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-09-11 20:49:08 -0400

    stage index route

--- controllers/stages_controller.js
+++ controllers/stages_controller.js
@@ -5,5 +5,16 @@ const { Stage } = db
 
 // ROUTES:
 
+// Find all Stages
+stages.get('/', async (req, res) => {
+    try {
+        const foundStages = await Stage.findAll()
+        res.status(200).json(foundStages)
+    } 
+    catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 // EXPORT
 module.exports = stages
